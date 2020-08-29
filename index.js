46cdedc4bf39b815ee49b6d7e5ff26d230d48fcf
Dmitry Kasporsky <31612563+KasporskiDzmitry@users.noreply.github.com>
2020-08-29 22:07:33 +0300

    add scroll bar and scroll to up button

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ toggle.addEventListener('click', ()=> {
         navbar.style.display = 'none';
     }
 });
+
+const progressBar = document.querySelector('.progress-bar');
+const scrollToTopButton = document.querySelector('.scrollToTopButton');
+
+window.addEventListener('scroll', () => {
+    let percent = Math.floor((window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100)
+    progressBar.style.width = `${percent}%`;
+
+    if (percent > 10) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
